5ab57ed9e1e49e76ad61ed28e7b3d4cf2aaabc9d
zhangchao2 <zhangchao2@asiainfo.com>
2016-05-10 14:58:59 +0800

    add 常量定义

--- route-core/src/main/java/com/ai/slp/route/constants/RouteConstant.java
+++ route-core/src/main/java/com/ai/slp/route/constants/RouteConstant.java
@@ -1,6 +1,6 @@
 package com.ai.slp.route.constants;
 
-public class RouteConstant {
+public static final class RouteConstant {
     /**
      * 新增
      */
@@ -20,5 +20,33 @@ public class RouteConstant {
      * 状态修改
      */
     public static final String S = "S";
+
+    public static final class Route {
+
+        public static final class State {
+
+            // 1新增
+            public static final String NEW = "1";
+
+            // 2正常
+            public static final String NORMAL = "2";
+
+            // 21预警
+            public static final String ALARM = "21";
+
+            // 3异常
+            public static final String ABNORMITY = "3";
+
+            // 5手动暂停
+            public static final String MANUAL_PAUSED = "5";
+
+            // 51自动暂停
+            public static final String AUTO_PAUSED = "51";
+
+            // 6废弃
+            public static final String ABANDONED = "6";
+
+        }
+    }
 
 }
