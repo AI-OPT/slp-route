ac37d76a6203d7efc5b333834c9235df9777fa60
liutong <liutong6053@163.com>
2016-07-04 14:44:47 +0800

    修改公共缓存调用方法

--- route-core/src/main/java/com/ai/slp/route/util/CacheDic.java
+++ route-core/src/main/java/com/ai/slp/route/util/CacheDic.java
@@ -3,13 +3,15 @@ package com.ai.slp.route.util;
 import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.ai.slp.common.api.cache.interfaces.ICacheSV;
 import com.ai.slp.common.api.cache.param.SysParam;
+import com.ai.slp.common.api.cache.param.SysParamSingleCond;
 
 public class CacheDic {
 
 
     public static String getAppKey(String tenantId, String typeCode, String paramCode) {
         ICacheSV iCacheSV = DubboConsumerFactory.getService("iCacheSV");
-        SysParam sysParam = iCacheSV.getSysParam(tenantId, typeCode, paramCode, "APPKEY");
+        SysParam sysParam = iCacheSV.getSysParamSingle(
+                new SysParamSingleCond(tenantId, typeCode, paramCode, "APPKEY"));
         return sysParam.getColumnDesc();
         //return "9db313541196135c37025f7a8021c4df";
     }
