9c5c0aefebf1896f583d2f8b2fa429890fc066c2
zhangzhongde <xtgjzzd@163.com>
2016-08-24 11:26:23 +0800

    修改注入

--- route-core/src/main/java/com/ai/slp/route/service/business/impl/IRouteItemBusiSVImpl.java
+++ route-core/src/main/java/com/ai/slp/route/service/business/impl/IRouteItemBusiSVImpl.java
@@ -3,13 +3,14 @@ package com.ai.slp.route.service.business.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ai.slp.route.service.atom.interfaces.IRouteItemAtomSV;
 import com.ai.slp.route.service.atom.interfaces.IRouteTargetAreaAtomSV;
 import com.ai.slp.route.service.business.interfaces.IRouteItemBusiSV;
 @Service
 public class IRouteItemBusiSVImpl implements IRouteItemBusiSV {
 
 	@Autowired
-	private IRouteItemBusiSV routeItemBusiSV;
+	private IRouteItemAtomSV routeItemAtomSV;
 	@Autowired
 	private IRouteTargetAreaAtomSV routeTargetAreaAtomSV;
 	
