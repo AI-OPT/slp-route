645d559296d5aac356436d073d2387f04bade4f0
gucl <gucl@asiainfo.com>
2016-10-09 11:08:07 +0800

    oneky

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,4 +1,11 @@
+git reset --hard origin/master 
 git pull 
+chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
 docker build -t 10.19.13.18:5000/slp-route:v1.0 .   
 docker push 10.19.13.18:5000/slp-route:v1.0 
+
+docker rmi aioptapp/slp-route:v1.0 
+docker tag 10.19.13.18:5000/slp-route:v1.0 aioptapp/slp-route:v1.0 
+docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
+docker push aioptapp/slp-route:v1.0 
