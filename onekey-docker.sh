1946e0947f81d4ff4c65829b53e73cce7e2a91fc
gucl <gucl@asiainfo.com>
2016-10-09 19:28:25 +0800

    tagversion=v1.0

--- onekey-docker.sh
+++ onekey-docker.sh
@@ -1,11 +1,12 @@
+tagversion=v1.0
 git reset --hard origin/master 
 git pull 
 chmod a+x onekey-docker.sh 
 gradle clean && gradle build -x test 
-docker build -t 10.19.13.18:5000/slp-route:v1.0 .   
-docker push 10.19.13.18:5000/slp-route:v1.0 
+docker build -t 10.19.13.18:5000/slp-route:${tagversion} .   
+docker push 10.19.13.18:5000/slp-route:${tagversion} 
 
-docker rmi aioptapp/slp-route:v1.0 
-docker tag 10.19.13.18:5000/slp-route:v1.0 aioptapp/slp-route:v1.0 
+docker rmi aioptapp/slp-route:${tagversion} 
+docker tag 10.19.13.18:5000/slp-route:${tagversion} aioptapp/slp-route:${tagversion} 
 docker login --username=aioptapp --password=aioptapp@123 --email=wuzhen3@asiainfo.com 
-docker push aioptapp/slp-route:v1.0 
+docker push aioptapp/slp-route:${tagversion} 
