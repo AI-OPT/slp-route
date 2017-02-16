26dc7886f961b8f7216bee1ac82d413fafc890dc
fazhicao <409993207@qq.com>
2017-02-16 16:11:05 +0800

    添加注释

--- route-core/src/main/java/com/ai/slp/route/util/SequenceUtil.java
+++ route-core/src/main/java/com/ai/slp/route/util/SequenceUtil.java
@@ -26,23 +26,27 @@ public final class SequenceUtil {
     private static final String ROUTE_ITEM$ROUTE_ITEM_ID$SEQ = "ROUTE_ITEM$ROUTE_ITEM_ID$SEQ";
     //路由区域标识
     private static final String ROUTE_TARGET_AREA$ROUTE_AREA_ID$SEQ = "ROUTE_TARGET_AREA$ROUTE_AREA_ID$SEQ";
-    
+    //创建仓库id
     public static String createRouteId() {
         return SeqUtil.getNewId(ROUTE$ROUTE_ID$SEQ, 16);
     }
+    //创建供应品添加日志id
     public static String createSupplyAddsLogId() {
         return SeqUtil.getNewId(ROUTE_SUPPLY_ADDS_LOG$SUPPLY_ADDS_LOG_ID$SEQ, 16);
     }
+    //创建供应品id
     public static String createSupplyId() {
         return SeqUtil.getNewId(ROUTE_PROD_SUPPLY$SUPPLY_ID$SEQ, 16);
     }
+    //创建仓库组id
     public static String createRouteGroupId(){
     	return SeqUtil.getNewId(ROUTE_GROUP$ROUTE_GROUP_ID$SEQ, 16);
     }
+    //创建路由组组成id
     public static String createRouteItemId(){
     	return SeqUtil.getNewId(ROUTE_ITEM$ROUTE_ITEM_ID$SEQ, 16);
     }
-    
+    //创建仓库区域id
     public static String createRouteAreaId(){
     	return SeqUtil.getNewId(ROUTE_TARGET_AREA$ROUTE_AREA_ID$SEQ, 12);
     }
